3f655be7060684bba7f1969affcd566fe7aaad6e
Lauren Giordano <laurengiordano94@gmail.com>
2023-07-11 12:24:16 +1000

    done with lab

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,15 @@
 require_relative './stack'
 
 # your code here
+
+def balancing_parentheses(str)
+    stack = Stack.new
+    str.each_char do |char|
+        if stack.peek == "(" && char == ")"
+            stack.pop
+        else
+            stack.push(char)
+        end
+    end
+    stack.size
+end
